1007f889e074b46f12a4bd8b6b6b50b750758bad
Sage Gerard <sage@sagegerard.com>
2020-02-18 13:18:21 -0500

    Add package description

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base" "parsack"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/syntax-highlighting.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "General purpose syntax highlighting for Racket")
 (define version "0.0")
 (define pkg-authors '(sage))
